f24c4456b259e00e9d74460ff2e9e33d8705b185
bill <you@example.com>
2020-12-27 18:01:00 +0900

    Brower Router 실습

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1> 소개  </h1>
+            <p>리액트 라우트를 실습하는 프로젝트입니다.</p>
+        </div>
+    )
+}
+
+export default About
