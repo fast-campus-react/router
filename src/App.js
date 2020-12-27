b6c607f5dc39842ce2e45ef2de4730e38bc4993f
bill <you@example.com>
2020-12-27 18:36:11 +0900

    history 관련 기능, block 기능 실습

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
   return (
@@ -17,10 +18,14 @@ function App() {
           <li>
             <Link to="/profiles">프로필 목록 </Link>
           </li>
+          <li>
+            <Link to="/history">히스토리  </Link>
+          </li>
         </ul>
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
         <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
       </BrowserRouter>
     </div>
   );
